c68364e01940af4726594aa8710cffce85b0c20c
Muhammad Talha <muhammadtalha04@gmail.com>
2021-04-09 17:39:37 +0500

    Moved the slices generation function from circle component to utils

--- src/components/Circle/Circle.tsx
+++ src/components/Circle/Circle.tsx
@@ -1,38 +1,22 @@
 import React, { useEffect, useMemo } from 'react';
 import { useConfigurationContext } from '../../contexts/ConfigurationContext';
 import { ConfActionTypes } from '../../types';
-import { getCoordinatesForPercent } from '../../utils';
-import { CircleWrapper, Path, Svg, SvgText } from './Style';
+import { generateSlices } from '../../utils';
+import { CircleWrapper, Svg, SvgText } from './Style';
 
 const Circle: React.FC = () => {
     const { state, dispatch } = useConfigurationContext();
 
-    const paths: JSX.Element[] = [];
     const textPaths: JSX.Element[] = [];
 
     // Create slices
     const render = useMemo(() => {
-        let cumulativePercent: number = 0;
+        const { paths, textPaths: tP } = generateSlices(state.numOfSlices, state.slicePercentage, state.intervalId, state.sequence, state.counter, state.color);
 
-        for (let i = 0; i < state.numOfSlices; i++) {
-            const [startX, startY] = getCoordinatesForPercent(cumulativePercent);
-            cumulativePercent += state.slicePercentage;
-            const [endX, endY] = getCoordinatesForPercent(cumulativePercent);
-
-            // If the slice is more than 50%, take the large arc (the long way around)
-            const largeArcFlag: number = state.slicePercentage > .5 ? 1 : 0;
-
-            const pathData: string = `M ${startX} ${startY} A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY} L 0 0`;
-
-            const color: string = (state.intervalId !== -1 && state.sequence[state.counter] === `${i}`) ? state.color : "transparent";
-
-            // Create slices and text
-            paths.push(<Path d={pathData} fill={color} key={i} id={`slice${i}`} />);
-            textPaths.push(<textPath key={i} href={`#slice${i}`} textAnchor="middle" startOffset="20%">{i + 1}</textPath>);
-        }
+        textPaths.push(...tP);
 
         return paths;
-    }, [paths, textPaths, state.color, state.numOfSlices, state.sequence, state.intervalId, state.slicePercentage]);
+    }, [textPaths, state.color, state.numOfSlices, state.sequence, state.intervalId, state.slicePercentage]);
 
     // On component mount
     useEffect(() => {
---
+++ src/utils/index.tsx
@@ -0,0 +1,65 @@
+import { Path } from '../components/Circle/Style';
+
+// Calculates the x and y coordinates to draw a slice
+export const getCoordinatesForPercent = (percent: number) => {
+    const x = Math.cos(2 * Math.PI * percent);
+    const y = Math.sin(2 * Math.PI * percent);
+    return [x, y];
+}
+
+// Generates random indexes to blink
+export const generateSequence = (numSlices: number): string[] => {
+    const sequence: string[] = [];
+
+    for (let i = 0; i < numSlices;) {
+        const randNum: number = Math.floor(Math.random() * numSlices);
+
+        if (sequence.indexOf(`${randNum}`) === -1) {
+            sequence.push(`${randNum}`);
+            i++;
+        }
+    }
+
+    return sequence;
+}
+
+// Matches the two given sequences to check if they are equal
+export const matchSequences = (sequence1: string[], sequence2: string[]) => {
+    if (sequence1.length !== sequence2.length) {
+        return false;
+    } else {
+        for (let i = 0; i < sequence1.length; i++) {
+            if (sequence1[i] !== sequence2[i]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+// Generates slices of a circle
+export const generateSlices = (numOfSlices: number, slicePercentage: number, intervalId: number, sequence: string[], counter: number, color: string) => {
+    let cumulativePercent: number = 0;
+    const paths: JSX.Element[] = [];
+    const textPaths: JSX.Element[] = [];
+
+    for (let i = 0; i < numOfSlices; i++) {
+        const [startX, startY] = getCoordinatesForPercent(cumulativePercent);
+        cumulativePercent += slicePercentage;
+        const [endX, endY] = getCoordinatesForPercent(cumulativePercent);
+
+        // If the slice is more than 50%, take the large arc (the long way around)
+        const largeArcFlag: number = slicePercentage > .5 ? 1 : 0;
+
+        const pathData: string = `M ${startX} ${startY} A 1 1 0 ${largeArcFlag} 1 ${endX} ${endY} L 0 0`;
+
+        const fillColor: string = (intervalId !== -1 && sequence[counter] === `${i}`) ? color : "transparent";
+
+        // Create slices and text
+        paths.push(<Path d={pathData} fill={fillColor} key={i} id={`slice${i}`} />);
+        textPaths.push(<textPath key={i} href={`#slice${i}`} textAnchor="middle" startOffset="20%" > {i + 1}</textPath>);
+    }
+
+    return { paths: paths, textPaths: textPaths };
+}
